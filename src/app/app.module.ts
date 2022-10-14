e2f5428a74380adfdb6d9765e1ea235a63fba8ac
aravindsabu <aravindsabu2001@gmail.com>
2022-10-14 14:27:15 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiginComponent } from './sigin/sigin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiginComponent
   ],
   imports: [
     BrowserModule,
