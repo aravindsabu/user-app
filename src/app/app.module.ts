77212016b9af652d76cc1bdc1a3c44d3c8e3494b
aravindsabu <aravindsabu2001@gmail.com>
2022-10-14 15:05:26 +0530

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SiginComponent } from './sigin/sigin.component';
+import { SiginupComponent } from './siginup/siginup.component';
+const appRoutes:Routes=[
+  {
+    path:"",component:SiginupComponent
+  },
+  {
+    path:"ok",component:SiginComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiginComponent
+    SiginComponent,
+    SiginupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
